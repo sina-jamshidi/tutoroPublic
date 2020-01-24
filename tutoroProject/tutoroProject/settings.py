3cb907953486ada5edc9ddb6e59aa82cb4d7fddb
Sina Jamshidi <hello@sinajamshidi.com>
2020-01-24 01:43:16 -0800

    update django settings

--- tutoroProject/tutoroProject/settings.py
+++ tutoroProject/tutoroProject/settings.py
@@ -16,7 +16,8 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
