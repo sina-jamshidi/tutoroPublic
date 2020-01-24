e445fb798d3ddeefaa08612c572b61f04312feb2
Sina Jamshidi <hello@sinajamshidi.com>
2020-01-24 01:23:04 -0800

    edit settings

--- tutoroProject/tutoroProject/settings.py
+++ tutoroProject/tutoroProject/settings.py
@@ -15,7 +15,8 @@ from datetime import timedelta
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -142,7 +143,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 PASSWORD_RESET_TIMEOUT_DAYS = 1
 
