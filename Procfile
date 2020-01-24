b32be75ed200faf6f98790189852af5d2a6eeaf2
Sina Jamshidi <hello@sinajamshidi.com>
2020-01-24 14:33:31 -0800

    working on procfile wsgi

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn tutoroProject.wsgi
+web: gunicorn tutoroProject/wsgi.wsgi:application
 web: python tutoroProject/manage.py runserver
 release: python tutoroProject/manage.py migrate
--- tutoroProject/tutoroProject/settings.py
+++ tutoroProject/tutoroProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tutorMatching',
+    'gunicorn',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
