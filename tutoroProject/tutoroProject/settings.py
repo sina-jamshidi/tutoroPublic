9a20eaec0b84acfa9618bea90314bbfc1f784e5f
Sina Jamshidi <hello@sinajamshidi.com>
2020-01-24 01:24:21 -0800

    update base_dir

--- tutoroProject/tutoroProject/settings.py
+++ tutoroProject/tutoroProject/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
